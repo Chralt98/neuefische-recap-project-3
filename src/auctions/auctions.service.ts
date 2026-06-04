d368bd4fb412923804c850849ff4435245d05838
Valentyn Beznosko <valikbeznosko@gmail.com>
2026-06-04 14:33:49 +0200

    refactor: improve bid validation logic and clean up imports in AuctionsService

--- src/auctions/auctions.service.ts
+++ src/auctions/auctions.service.ts
@@ -1,15 +1,15 @@
 import {
-  ConflictException,
-  Injectable,
-  NotFoundException,
+    ConflictException,
+    Injectable,
+    NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { plainToInstance } from 'class-transformer';
 import { Repository } from 'typeorm';
-import { CreateAuctionDto } from './dto/create-auction.dto';
-import { Auction } from './entities/auction.entitiy';
 import { OfferResponseDto } from '../offers/dto/offer-reponse.dto';
 import { OffersService } from '../offers/offers.service';
-import { plainToInstance } from 'class-transformer';
+import { CreateAuctionDto } from './dto/create-auction.dto';
+import { Auction } from './entities/auction.entitiy';
 
 @Injectable()
 export class AuctionsService {
@@ -48,24 +48,17 @@ export class AuctionsService {
     price: number,
   ): Promise<OfferResponseDto> {
     const auction = await this.auction.findOneBy({ id: auctionId });
+    
     if (!auction) {
       throw new NotFoundException(`Auction with id ${auctionId} not found`);
     }
+      
     if (price < auction.currentPrice) {
       throw new ConflictException(
-        'Bid price must be higher than current price',
+        'Bid price must be higher than current or starting price',
       );
     }
 
-    if (auction.offers.length === 0 && auction.startingPrice >= price) {
-      throw new ConflictException(
-        'Bid price must be higher than starting price',
-      );
-    } else if (auction.offers.length > 0 && auction.currentPrice >= price) {
-      throw new ConflictException(
-        'Bid price must be higher than current price',
-      );
-    }
     if (auction.endDate.getTime() < Date.now()) {
       throw new ConflictException('Auction has already ended');
     }
